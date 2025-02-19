2dad9a3de4b70530c53e230c996c0d17a2301169
Subash Chhetri <subash23000@student.hamk.fi>
2025-02-19 21:12:53 +0200

    added header, footer and main part of my website

---
+++ login/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?php echo $title; ?>
+    </title>
+    <style>
+
+    </style>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="" alt="Himalayan Cafe Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">LOCATION</a></li>
+                <li><a href="#">REVIEW</a></li>
+                <li><a href="#">CONTACT</a></li>
+                <li><a href="#">LOGIN</a></li>
+            </ul>
+        </nav>
+    </header>
+    
---
+++ login/main.php
@@ -0,0 +1,13 @@
+<?php
+// including the header.php
+$title = "My Title";
+include 'header.php';
+?>
+
+<main>
+
+</main>
+
+<?php
+include 'footer.php';
+?>
